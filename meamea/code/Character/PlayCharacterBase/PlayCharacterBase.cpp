224cadbcd6a6e54accba266aa5a0d359274bbe73
Koutexya <>
2024-06-10 20:27:25 +0900

    0610伊藤

---
+++ meamea/code/Character/PlayCharacterBase/PlayCharacterBase.cpp
@@ -0,0 +1,29 @@
+#include"PlayCharacterBase.h"
+
+namespace mea
+{
+	PlayCharacterBase::PlayCharacterBase()
+	{
+
+	}
+
+	PlayCharacterBase::~PlayCharacterBase()
+	{
+
+	}
+
+	void PlayCharacterBase::Update()
+	{
+
+	}
+
+	void PlayCharacterBase::Draw()
+	{
+
+	}
+
+	void PlayCharacterBase::Input(float deltaTime)
+	{
+
+	}
+}
