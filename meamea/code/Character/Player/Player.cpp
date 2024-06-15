fa1685893e10cc7b19ad6e5b2f67a4e9d94e4960
406-09\student <fca>
2024-06-15 16:03:40 +0900

    0615神野

--- meamea/code/Character/Player/Player.cpp
+++ meamea/code/Character/Player/Player.cpp
@@ -1,2 +1,42 @@
 #include"Player.h"
 #include"DxLib.h"
+
+namespace mea
+{
+    Player::Player()//コンストラクタ
+    {
+
+    }
+    Player::~Player()//デストラクタ
+    {
+
+    }
+
+    void Player::Update()//更新
+    {
+
+    }
+    void Player::Draw()//描画
+    {
+        
+    }
+    
+    bool Player::IsMove(int keytype)//動けるかどうか
+    {
+        //入力された方向のマップチップを見る
+        //上
+        //下
+        //右
+        //左
+        
+        //動く壁があるときmapobjectの関数呼ぶ
+        //さらに横を見る
+        
+        //床または罠のとき移動
+
+        return true;
+    }
+
+    //trueのとき動かす
+
+}
--- meamea/code/Character/Player/Player.h
+++ meamea/code/Character/Player/Player.h
@@ -1,1 +1,15 @@
-#pragma once
+#include"DxLib.h"
+
+namespace mea
+{
+    class Player
+    {
+    public:
+        Player();//コンストラクタ
+        ~Player();//デストラクタ
+
+        void Update();//更新
+        void Draw();//描画
+        bool IsMove(int keytype);//動けるかどうか
+    };
+}
