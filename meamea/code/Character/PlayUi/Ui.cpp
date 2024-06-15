af41cf08254f6a42174f127a343e24f5e37eccb5
406-08\student <fca>
2024-06-15 12:18:43 +0900

    0615伊藤

---
+++ meamea/code/Character/PlayUi/Ui.cpp
@@ -0,0 +1,29 @@
+#include "PlayUi.h"
+
+namespace mea
+{
+    PlayUi::PlayUi()
+    {
+
+    }
+
+    PlayUi::~PlayUi()
+    {
+
+    }
+
+    void PlayUi::Update()
+    {
+
+    }
+
+    void PlayUi::Draw()
+    {
+
+    }
+
+    void PlayUi::LoadFont()
+    {
+
+    }
+}
--- meamea/code/Character/Stage/Stage.h
+++ meamea/code/Character/Stage/Stage.h
@@ -1,44 +1,51 @@
 #pragma once
 #include<Dxlib>
 
-class Stage final
+namespace mea
 {
-public:
+    class Stage final
+    {
+    public:
 
-    /// <summary>
-    /// コンストラクタ
-    /// </summary>
-    Stage();
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Stage();
 
-    /// <summary>
-    /// デストラクタ
-    /// </summary>
-    ~Stage();
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~Stage();
 
-    /// <summary>
-    /// 更新処理
-    /// </summary>
-    void Update()override;
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()override;
 
-    /// <summary>
-    /// 描画処理
-    /// </summary>
-    void Draw()override;;
+        /// <summary>
+        /// 描画処理
+        /// </summary>
+        void Draw()override;;
 
-    /// <summary>
-    /// マップ情報を返す
-    /// </summary>
-    /// <param name="x">x値</param>
-    /// <param name="y">y値</param>
-    /// <returns></returns>
-    int GetMapnumber(int x,int y)const { return mMapnumber[x][y]; }
+        /// <summary>
+        /// マップ情報を返す
+        /// </summary>
+        /// <param name="x">x値</param>
+        /// <param name="y">y値</param>
+        /// <returns></returns>
+        int GetMapnumber(int x, int y)const { return mMapnumber[x][y]; }
 
-private:
+    private:
 
-    const int mMapnumber[1][1];    //マップ情報
+        int mMapnumber1[8][8];    //マップ情報
+        int mMapnumber2[16][16];    //マップ情報
+        int mMapnumber3[24][24];    //マップ情報
 
-    int mMapChip;                  //マップ画像
-    int mCharacterL;               //装飾用画像左
-    int mCharacterR;               //装飾用画像右
-};
 
+        int mMapChip;                  //マップ画像
+        int mCharacterL;               //装飾用画像左
+        int mCharacterR;               //装飾用画像右
+    };
+
+
+}
