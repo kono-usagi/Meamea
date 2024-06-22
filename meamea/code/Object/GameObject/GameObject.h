745298f979976434f5e7b9cec51e48faa6ea1ac8
406-01\student <kazushi6kana11@gmail.com>
2024-06-22 16:29:00 +0900

    GameObjectの編集

--- meamea/code/Object/GameObject/GameObject.h
+++ meamea/code/Object/GameObject/GameObject.h
@@ -1,5 +1,6 @@
 #include"DxLib.h"
 #include"PlayObjectTag.h"
+#include<vector>
 
 
 namespace mea
@@ -10,20 +11,51 @@ namespace mea
     class GameObject
     {
     public:
-        //vectorに変更の可能性あり
-        int mX;//オブジェクトのx座標
-        int mY;//オブジェクトのy座標
-        GameObject(PlayObjectTag tag);//コンストラクタ
+        std::vector<int> mPos = { 0,0 };
+        /// <summary>
+        /// オブジェクトのx座標
+        /// </summary>
+        int mX;
+        /// <summary>
+        /// オブジェクトのy座標
+        /// </summary>
+        int mY;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="tagNasme"></param>
+        GameObject(std::string tagNasme);
         //GameObject(PlayObjectTag tag, VECTOR pos);//コンストラクタ
 
-        virtual ~GameObject();//デストラクタ
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        virtual ~GameObject();
 
-        virtual void Update();//更新
-        virtual void Draw();//描画
-        virtual void Input(float deltatime);//操作処理
+        /// <summary>
+        /// 更新
+        /// </summary>
+        virtual void Update(float deltatime) = 0;
+
+        /// <summary>
+        /// 描画
+        /// </summary>
+        virtual void Draw() = 0;
+
+        /// <summary>
+        /// 操作処理
+        /// </summary>
+        /// <param name="deltatime"></param>
+        virtual void Input() = 0;
+
+        /// <summary>
+        /// タグ名取得
+        /// </summary>
+        /// <returns></returns>
+        std::string GetTagName() { return tagName; }
 
     private:
-
-
+        std::string tagName;
     };
 }
