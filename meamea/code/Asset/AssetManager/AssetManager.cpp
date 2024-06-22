c9e22b7aaf597770f31b5a71caacbd5d801e604b
yama <Reo953193@gmail.com>
2024-06-22 23:40:55 +0900

    yama

--- meamea/code/Asset/AssetManager/AssetManager.cpp
+++ meamea/code/Asset/AssetManager/AssetManager.cpp
@@ -1,1 +1,17 @@
 #include "AssetManager.h"
+
+namespace mea
+{
+	AssetManager::AssetManager()
+	{
+		//ˆ—‚È‚µ
+	}
+
+	AssetManager::~AssetManager()
+	{
+		//ƒnƒ“ƒhƒ‹‚Ìíœ
+		img->DeleteHandle();
+		sound->DeleteHandle();
+	}
+}
+
