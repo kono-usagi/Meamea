f4a00e29f2074ac20b6faf6bd3ccd94ec99041ce
406-18\student <fca>
2024-07-09 16:19:30 +0900

    Playerの編集

--- meamea/code/Object/Play/Player/Player.h
+++ meamea/code/Object/Play/Player/Player.h
@@ -1,24 +1,56 @@
 #include"DxLib.h"
 #include"../../GameObject/GameObject.h"
 
+const int ANIM_PATTERN_NUM = 2;//アニメーションパターン数　右向きと左向き
+const int ANIM_LEFT = 0;//左
+const int ANIM_RIGHT = 1;//右
+const int ANIM_TYPE_NUM = 5;//アニメーションの種類
+
+const float ANIM_FPS = 6.0f;//秒間6コマのアニメーション
+const int IMAGE_SIZE_X = 50;//画像たてサイズ
+const int IMAGE_SIZE_Y = 50;//画像よこサイズ
+
+const int PLY_DISTANCE = 50;//プレイヤー移動距離　50
+//const int CHIP_SIZE = 50;//チップサイズ　50
+
+float animTimer;//アニメーションタイマー
+int animNowPattern;//現在のアニメパターン
+int animNowType;//現在のアニメ種類
+int animNowIndex;//アニメーション画像配列の添え字
+
 namespace mea
 {
     class Player:GameObject
     {
     public:
-        const int PlyDistance = 50;//プレイヤー移動距離　50
-        //const int ChipSize = 50;//チップサイズ　50
-        const int PlyImg = 50;//プレイヤー画像
-        const int PlyType = 5;//プレイヤー画像の種類　5
-        int PlyDir = 0;//プレイヤーの向き　左0　右1
-        
+        int mPlayerHandle[ANIM_PATTERN_NUM * ANIM_TYPE_NUM];//プレイヤーの画像ハンドル
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Player();
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~Player();
 
-        Player();//コンストラクタ
-        ~Player();//デストラクタ
-
-        virtual void Update();//更新
-        virtual void Draw();//描画
-        virtual void Move(int keyType);//移動
+        /// <summary>
+        /// 更新
+        /// </summary>
+        virtual void Update();
+        /// <summary>
+        /// 描画
+        /// </summary>
+        virtual void Draw();
+        /// <summary>
+        /// アニメーション処理
+        /// </summary>
+        /// <param name="deltaTime"></param>
+        virtual void Animation(float deltaTime);
+        /// <summary>
+        /// 移動
+        /// </summary>
+        /// <param name="keyType"></param>
+        virtual void Move(int keyType);
     };
 }
