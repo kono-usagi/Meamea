7b8e6b2136b5535f771475017f903cddcd42b5d3
406 <>
2024-07-12 10:06:08 +0900

    m

--- meamea/code/Asset/AssetManager/AssetManager.cpp
+++ meamea/code/Asset/AssetManager/AssetManager.cpp
@@ -2,16 +2,27 @@
 
 namespace mea
 {
+	std::unique_ptr<AssetManager>AssetManager::assetmanager = nullptr;
+
 	AssetManager::AssetManager()
+		:imagejson(new ImageJson)
 	{
 		//処理なし
 	}
 
+	void AssetManager::InitAssetManager()
+	{
+		//実態が空だったらインスタンス生成
+		if (!assetmanager)
+		{
+			assetmanager.reset(new AssetManager);
+		}
+	}
+
 	AssetManager::~AssetManager()
 	{
-		//ハンドルの削除
-		img->DeleteHandle();
-		sound->DeleteHandle();
+		//解放
+		imagejson->DeleteHandle();
 	}
 }
 
