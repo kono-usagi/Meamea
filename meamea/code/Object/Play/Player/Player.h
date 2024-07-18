382ba6d412473d8070550e9804d675d3c2705b4f
406 <>
2024-07-18 19:46:10 +0900

    Playerクラスの編集

--- meamea/code/Object/Play/Player/Player.h
+++ meamea/code/Object/Play/Player/Player.h
@@ -1,6 +1,8 @@
 #include"DxLib.h"
 #include"../../GameObject/GameObject.h"
+#include"../Stage/Stage.h"
 
+//アニメーション定数
 const int ANIM_PATTERN_NUM = 2;//アニメーションパターン数　右向きと左向き
 const int ANIM_LEFT = 0;//左
 const int ANIM_RIGHT = 1;//右
@@ -13,6 +15,19 @@ const int IMAGE_SIZE_Y = 50;//画像よこサイズ
 const int PLY_DISTANCE = 50;//プレイヤー移動距離　50
 //const int CHIP_SIZE = 50;//チップサイズ　50
 
+
+
+
+//キー入力定数
+const int KEY_UP = 0;
+const int KEY_DOWN = 1;
+const int KEY_RIGHT = 2;
+const int KEY_LEFT = 3;
+
+//その他定数
+
+
+//アニメーション変数　メンバ変数？
 float animTimer;//アニメーションタイマー
 int animNowPattern;//現在のアニメパターン
 int animNowType;//現在のアニメ種類
@@ -47,6 +62,13 @@ namespace mea
         /// </summary>
         void Input()override;
         /// <summary>
+        /// 動けるかどうか
+        /// </summary>
+        /// <returns></returns>
+        bool isMove(int keyType)override;
+
+
+        /// <summary>
         /// アニメーション処理
         /// </summary>
         /// <param name="deltaTime"></param>
@@ -55,6 +77,6 @@ namespace mea
         /// 移動
         /// </summary>
         /// <param name="keyType"></param>
-        void Move(int keyType);
+        void Move(int keyType)override;
     };
 }
