61fbcef9a83267d8662ad94ea65afc96186431b5
Koutexya <>
2024-07-22 03:20:59 +0900

    StoryUiの内容書き込み

--- meamea/code/Object/Story/UI/Ui.cpp
+++ meamea/code/Object/Story/UI/Ui.cpp
@@ -2,84 +2,217 @@
 
 namespace mea
 {
-    Ui::Ui()
+    void Ui::Init()
     {
+        //画像読み込み
+        ChoiceImg = LoadGraph("img/Choice.PNG");
+        ChoiceDecisionImg = LoadGraph("img/Choice_decision.PNG");
+
+        //フォントのロード
+        if (AddFontResourceEx(fontPath, FR_PRIVATE, NULL) > 0) {}
+        else
+        {
+            //フォント読み込みエラー
+            MessageBox(NULL, "フォント読み込み失敗", "", MB_OK);
+        }
+        ChangeFont("BIZ UDPMincho", DX_CHARSET_DEFAULT);
+
         //ファイル読み込み
-        mFileHandle = FileRead_open("File名");
-        //フォント読み込み
-        mFontPath = "フォントファイル名.ttf";
+        FileHandle = FileRead_open(FileName[0]);
+
+        //フォントサイズ設定
+        SetFontSize(46);
     }
 
     Ui::~Ui()
     {
-        //ファイルを閉じる
-        FileRead_close(mFileHandle);
+        //画像の削除
+        DeleteGraph(ChoiceImg);
+        DeleteGraph(ChoiceDecisionImg);
 
         //フォントのアンロード
-        if (RemoveFontResourceEx(mFontPath, FR_PRIVATE, NULL)) {}
+        if (RemoveFontResourceEx(fontPath, FR_PRIVATE, NULL)) {
+        }
+        else {
+            MessageBox(NULL, "フォントのアンロードに失敗", "", MB_OK);
+        }
+
+        //ファイルを閉じる
+        FileRead_close(FileHandle);
+    }
+
+    void Ui::Input()
+    {
+        //テキストを送る入力
+        if (CheckHitKey(KEY_INPUT_SPACE))
+        {
+            if (KeyFlag == 0)
+            {
+                FileRead_gets(TextFlag, 256, FileHandle);//一行読み込み
+
+
+
+                if (strcmp(TextFlag, TFlag) == 0)
+                {
+                    Q = false;
+                    FileRead_gets(NameString, 256, FileHandle);//一行読み込み
+                    NameWidth = GetDrawStringWidth(NameString, -1);//文字列の幅を取得
+                    if (ChoiceDecisionY == 750)
+                    {
+                        FileRead_gets(TextString1, 256, FileHandle);//一行読み込み
+                        TextWidth1 = GetDrawStringWidth(TextString1, -1);//文字列の幅を取得
+                    }
+                    else if (ChoiceDecisionY == 900)
+                    {
+                        FileRead_gets(TextString2, 256, FileHandle);//一行読み込み
+                        TextWidth2 = GetDrawStringWidth(TextString2, -1);//文字列の幅を取得
+                    }
+                }
+                else if (strcmp(TextFlag, QFlag) == 0)
+                {
+                    Q = true;
+                    FileRead_gets(NameString, 256, FileHandle);//一行読み込み
+                    NameWidth = GetDrawStringWidth(NameString, -1);//文字列の幅を取得
+                    FileRead_gets(TextString1, 256, FileHandle);//一行読み込み
+                    TextWidth1 = GetDrawStringWidth(TextString1, -1);//文字列の幅を取得
+                    FileRead_gets(TextString2, 256, FileHandle);//一行読み込み
+                    TextWidth2 = GetDrawStringWidth(TextString2, -1);//文字列の幅を取得
+                }
+                KeyFlag = 1;
+            }
+        }
         else
         {
-            //フォントのアンロードエラー表示   
-            MessageBox(NULL, "フォントのアンロード失敗", "", MB_OK);
+            KeyFlag = 0;
+        }
+
+        if (Q == true)
+        {
+            if (CheckHitKey(KEY_INPUT_UP))
+            {
+                if (ChoiceButton == 0)
+                {
+                    ChoiceButton = 1;
+                    ChoiceY = 900;
+                    ChoiceDecisionY = 750;
+                }
+            }
+            else
+            {
+                ChoiceButton = 0;
+            }
+
+            if (CheckHitKey(KEY_INPUT_DOWN))
+            {
+                if (ChoiceButton == 0)
+                {
+                    ChoiceButton = 1;
+                    ChoiceY = 750;
+                    ChoiceDecisionY = 900;
+                }
+            }
+            else
+            {
+                ChoiceButton = 0;
+            }
+
+            if (CheckHitKey(KEY_INPUT_RETURN))
+            {
+                Q = false;
+                if (ChoiceDecisionY == 750)
+                {
+                    FileRead_gets(NextFile, 256, FileHandle);//一行読み込み
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (strcmp(NextFile, FileName[i]) == 0)
+                        {
+                            FileRead_close(FileHandle);
+                            FileHandle = FileRead_open(FileName[i]);
+                        }
+                    }
+                }
+                else if (ChoiceDecisionY == 900)
+                {
+                    FileRead_gets(NextFile, 256, FileHandle);
+                    FileRead_gets(NextFile, 256, FileHandle);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (strcmp(NextFile, FileName[i]) == 0)
+                        {
+                            FileRead_close(FileHandle);
+                            FileHandle = FileRead_open(FileName[i]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                ChoiceButton = 0;
+            }
+        }
+
+    }
+
+    void Ui::Draw()
+    {
+        if (Q == true)
+        {
+            DrawGraph((1920 - 1013) / 2, ChoiceY, ChoiceImg, TRUE);
+            DrawGraph((1920 - 1013) / 2, ChoiceDecisionY, ChoiceDecisionImg, TRUE);
+        }
+
+        if (flg == 0)
+        {
+            flg = 1;
+        }
+        else
+        {
+            if (Q == false)//前表示した名前を黒で塗りつぶす
+            {
+                if (ChoiceDecisionY == 750)
+                {
+                    DrawString(0, 0, TextFlag, GetColor(0, 0, 0));
+                    DrawString((342 - NameWidth) / 2, 660, NameString, GetColor(0, 0, 0));//前表示した名前を黒で塗りつぶす
+                    DrawString((1920 - TextWidth1) / 2, 900, TextString1, GetColor(0, 0, 0));//前表示した文字を黒で塗りつぶす
+                }
+            }
+            else
+            {
+                if (ChoiceDecisionY == 900)
+                {
+                    DrawString(0, 0, TextFlag, GetColor(0, 0, 0));
+                    DrawString((342 - NameWidth) / 2, 660, NameString, GetColor(0, 0, 0));
+                    DrawString((1920 - TextWidth1) / 2, 780, TextString1, GetColor(0, 0, 0));
+                    DrawString((1920 - TextWidth2) / 2, 930, TextString2, GetColor(0, 0, 0));
+                }
+            }
+            DrawString(0, 0, TextFlag, GetColor(255, 255, 255));//表示
+
+            if (Q == false)//文字の表示
+            {
+                DrawString((342 - NameWidth) / 2, 660, NameString, GetColor(255, 255, 255));//名前表示
+                if (ChoiceDecisionY == 750)
+                {
+                    DrawString((1920 - TextWidth1) / 2, 900, TextString1, GetColor(255, 255, 255));//セリフ表示
+                }
+                else if (ChoiceDecisionY == 900)
+                {
+                    DrawString((1920 - TextWidth2) / 2, 900, TextString2, GetColor(255, 255, 255));//セリフ表示
+                }
+                //ChoiceDecisionY = 750;
+            }
+            else
+            {
+                DrawString((342 - NameWidth) / 2, 660, NameString, GetColor(255, 255, 255));//名前表示
+                DrawString((1920 - TextWidth1) / 2, 780, TextString1, GetColor(255, 255, 255));//セリフ表示
+                DrawString((1920 - TextWidth2) / 2, 930, TextString2, GetColor(255, 255, 255));//セリフ表示
+            }
         }
     }
 
     void Ui::Update()
     {
+        Input();
         Draw();
-    }
-
-    void Ui::Draw()
-    {
-        while (FileRead_eof(mFileHandle) == 0)
-        {
-            if (CheckHitKey(KEY_INPUT_SPACE))
-            {
-                if (mKeyFlag == 0)
-                {
-                    if (mFlg == 0)
-                    {
-                        mFlg = 1;
-                    }
-                    else//最初の表示以外はここで前回の文字表示を消す
-                    {
-                        DrawString((342 - mNameWidth) / 2, 660, mNameString, GetColor(0, 0, 0));//前表示した名前を黒で塗りつぶす
-                        DrawString((1920 - mTextWidth) / 2, 900, mTextString, GetColor(0, 0, 0));//前表示した文字を黒で塗りつぶす
-
-                    }
-                    FileRead_gets(mNameString, 256, mFileHandle);//一行読み込み
-                    mNameWidth = GetDrawStringWidth(mNameString, -1);//文字列の幅を取得
-                    DrawString((342 - mNameWidth) / 2, 660, mNameString, GetColor(255, 255, 255));//名前表示
-
-                    FileRead_gets(mTextString, 256, mFileHandle);//一行読み込み
-                    mTextWidth = GetDrawStringWidth(mTextString, -1);//文字列の幅を取得
-                    DrawString((1920 - mTextWidth) / 2, 900, mTextString, GetColor(255, 255, 255));//セリフ表示
-
-                    mKeyFlag = 1;
-
-                }
-            }
-            else
-            {
-                mKeyFlag = 0;
-            }
-        }
-        mFlg = 0;
-    }
-
-    void Ui::LoadFont()
-    {
-        //フォントのロード
-        if (AddFontResourceEx(mFontPath, FR_PRIVATE, NULL) > 0) {}
-        else
-        {
-            //フォント読み込みエラー表示
-            MessageBox(NULL, "フォント読み込み失敗", "", MB_OK);
-        }
-        //フォント変更
-        ChangeFont("フォント名", DX_CHARSET_DEFAULT);
-
-        //文字サイズ変更
-        SetFontSize(36);
     }
 }
--- meamea/code/Object/Story/UI/Ui.h
+++ meamea/code/Object/Story/UI/Ui.h
@@ -8,14 +8,19 @@ namespace mea
     {
     public:
         /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        Ui();
-
-        /// <summary>
         /// デストラクタ
         /// </summary>
         ~Ui();
+
+        /// <summary>
+        /// 初期設定
+        /// </summary>
+        void Init();
+
+        /// <summary>
+        /// 入力処理
+        /// </summary>
+        void Input();
 
         /// <summary>
         /// 更新処理
@@ -27,19 +32,33 @@ namespace mea
         /// </summary>
         void Draw();
 
-        /// <summary>
-        /// フォントの設定
-        /// </summary>
-        void LoadFont();
 
         LPCSTR  mFontPath;
-        int mFileHandle;//ファイルの読み込み先
-        char mTextString[256];//一行分のセリフ
-        char mNameString[256];//話しているキャラクターの名前
-        int mTextWidth = 0;//セリフの幅
-        int mNameWidth = 0;//名前の幅
-        int mKeyFlag = 0;//入力した瞬間か
-        int mFlg = 0;
+
+        int FileHandle;//現在のファイル入れるとこ
+        char FileName[3][20] = { "Lilian_Talk1.txt","Lilian_Choice1A.txt","Lilian_Choice1B.txt" };//すべてのファイル名をここに保存
+        char NextFile[20];//次読み込むファイル
+        int KeyFlag = 0;//入力したら
+        int flg = 0;//最初の文字表示]
+
+        char TextString1[256];//テキスト保存
+        char TextString2[256];//テキスト保存
+        char NameString[256];//名前保存
+        int TextWidth1;//テキストの幅
+        int TextWidth2;//テキストの幅
+        int NameWidth;//名前の幅
+
+        char TextFlag[2] = "t";//テキストの種類判別用
+
+        char TFlag[2] = "t";//"t"保存
+        char QFlag[2] = "q";//"q"保存
+        bool Q = false;
+
+        int ChoiceImg;//選択をしていないほうの画像
+        int ChoiceDecisionImg;//選択をしている方の画像
+        int ChoiceButton;//今どちらを選択しているかを画像の位置保存
+        int ChoiceY = 900;//選択をしていないほうの画像位置
+        int ChoiceDecisionY = 750;//選択をしている方の画像位置
 
 
     };
