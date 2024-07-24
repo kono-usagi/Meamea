8f093eb6a6d686d8f85bae5b69dbd369e05fc1f1
yama <Reo953193@gmail.com>
2024-07-25 01:02:30 +0900

    Storyの修正

--- meamea/code/Scene/Story/Story.h
+++ meamea/code/Scene/Story/Story.h
@@ -8,10 +8,29 @@ namespace mea
     class Story :public SceneBase
     {
     public:
-        Story();//コンストラクタ
-        ~Story();//デストラクタ
 
-        SceneBase* Update(float deltaTime)override;
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        Story();
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~Story();
+
+    private:
+
+        /// <summary>
+        /// シーン更新処理
+        /// </summary>
+        /// <param name="deltaTime">デルタタイム</param>
+        /// <returns>次のフレームシーン</returns>
+        SceneBase* Update(const float deltaTime)override;
+
+        /// <summary>
+        /// シーン描画処理
+        /// </summary>
         void Draw()override;
     };
 }
