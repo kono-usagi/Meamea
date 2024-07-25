31696430fb8f4217434d9f73244a3088457139c2
408-22\student <fca22090018@edu.fca.ac.jp>
2024-07-25 16:46:21 +0900

    Stageクラスの編集

--- meamea/code/Object/Play/Stage/Stage.cpp
+++ meamea/code/Object/Play/Stage/Stage.cpp
@@ -7,6 +7,8 @@ namespace mea
         //マップ画像の読み込み
         //mMapImage=
 
+        //動く岩の描画
+
         //マップ情報の読み込み
         //mMapnumber=
 
@@ -19,6 +21,8 @@ namespace mea
 
     void Stage::Update()
     {
+        Draw();
+        Input();
     }
 
     void Stage::Draw()
@@ -27,28 +31,100 @@ namespace mea
         DrawGraph(0, 0, mMapImage, FALSE);
         
     }
+    void Stage::DrawMapObject()
+    {
+        //動く岩が移動したら
+
+
+    }
 
     void Stage::Input()
     {
-        if (CheckHitKey(KEY_INPUT_UP) == 1)//上
+        int keyFlg = 0;
+        if (CheckHitKey(KEY_INPUT_UP) == 1|| CheckHitKey(KEY_INPUT_DOWN) == 1|| CheckHitKey(KEY_INPUT_RIGHT) == 1|| CheckHitKey(KEY_INPUT_LEFT) == 1)
         {
-            isMove(KEY_UP);
+            keyFlg = 1;
+            if (CheckHitKey(KEY_INPUT_UP) == 1)//上
+            {
+                isPlayerMove(KEY_UP);
+            }
+            if (CheckHitKey(KEY_INPUT_DOWN) == 1)//下
+            {
+                isPlayerMove(KEY_DOWN);
+            }
+            if (CheckHitKey(KEY_INPUT_RIGHT) == 1)//右
+            {
+                isPlayerMove(KEY_RIGHT);
+            }
+            if (CheckHitKey(KEY_INPUT_LEFT) == 1)//左
+            {
+                isPlayerMove(KEY_LEFT);
+            }
         }
-        if (CheckHitKey(KEY_INPUT_DOWN) == 1)//下
+        else
         {
-            isMove(KEY_DOWN);
+            keyFlg = 0;
         }
-        if (CheckHitKey(KEY_INPUT_RIGHT) == 1)//右
+
+    }
+
+    void Stage::isPlayerMove(int keyType)
+    {
+        //現在地の配列取得
+
+        //入力方向の配列内情報の取得
+        if (keyType == KEY_UP)//上
         {
-            isMove(KEY_RIGHT);
+            mMapInfo=mMapInfoArray[mArrayFirst][mArraySecond - mMoveDistance];
         }
-        if (CheckHitKey(KEY_INPUT_LEFT) == 1)//左
+        if (keyType == KEY_DOWN)//下
         {
-            isMove(KEY_LEFT);
+            mMapInfo = mMapInfoArray[mArrayFirst][mArraySecond + mMoveDistance];
+        }
+        if (keyType == KEY_RIGHT)//右
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst + mMoveDistance][mArraySecond];
+        }
+        if (keyType == KEY_LEFT)//左
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst - mMoveDistance][mArraySecond];
+        }
+
+        if (mMapInfo == MAP_OBJECT_ROCK)//動く岩のとき
+        {
+            isMapObjectMove(keyType);
+        }
+        if (mMapInfo % 2 == 0)//情報が偶数のとき動かす　床0　罠2
+        {
+            Player::Move(keyType);
         }
     }
 
-    void Stage::isMove
+    void Stage::isMapObjectMove(int keyType)
+    {
+        //入力方向の配列内情報の取得
+        if (keyType == KEY_UP)//上
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst][mArraySecond - mMoveDistance-mMoveDistance];
+        }
+        if (keyType == KEY_DOWN)//下
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst][mArraySecond + mMoveDistance]+ mMoveDistance;
+        }
+        if (keyType == KEY_RIGHT)//右
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst + mMoveDistance+ mMoveDistance][mArraySecond];
+        }
+        if (keyType == KEY_LEFT)//左
+        {
+            mMapInfo = mMapInfoArray[mArrayFirst - mMoveDistance- mMoveDistance][mArraySecond];
+        }
+
+        if (mMapInfo % 2 == 0)//情報が偶数のとき動かす　床0　罠2
+        {
+            MapObject::Move(keyType);
+        }
+    }
 }
 
 
--- meamea/code/Object/Play/Stage/Stage.h
+++ meamea/code/Object/Play/Stage/Stage.h
@@ -5,10 +5,10 @@
 
 
 //配列サイズ定数
-const int FIRST_SIZE = 30;//マップ情報配列の最初の要素数
-const int SECOND_SIZE = 10;//マップ情報配列の二番目の要素数
+const int X_SIZE = 30;//マップ情報配列の最初の要素数
+const int Y_SIZE = 10;//マップ情報配列の二番目の要素数
 
-int mMapInfoArray[FIRST_SIZE][SECOND_SIZE];//マップ情報配列
+int mMapInfoArray[X_SIZE][Y_SIZE];//マップ情報配列
 int mArrayFirst;//マップ情報配列の最初
 int mArraySecond;//マップ情報配列の二番目
 int mMapInfo;//マップ情報種類
@@ -46,31 +46,40 @@ namespace mea
         void Input()override;
 
         /// <summary>
+        /// 動く岩の描画処理
+        /// </summary>
+        void DrawMapObject();
+
+        /// <summary>
         /// マップ情報を返す
         /// </summary>
         /// <param name="x">x値</param>
         /// <param name="y">y値</param>
         /// <returns></returns>
-        int GetMapNumber(int x, int y)const { return mMapNumber[x][y]; }
+        //int GetMapNumber(int x, int y)const { return mMapNumber[x][y]; }
+
+        /// <summary>
+        /// 動けるか判定
+        /// </summary>
+        /// <param name="keyType"></param>
+        //void isMove(int keyType);
 
         /// <summary>
         /// プレイヤーが移動できるか
         /// </summary>
-        /// <returns></returns>
-        bool isPlayerMove();
+        void isPlayerMove(int keyType);
 
         /// <summary>
         /// 動く岩が移動できるか
         /// </summary>
-        /// <returns></returns>
-        bool isMapObjectMove();
+        void isMapObjectMove(int keyType);
 
     private:
         //マップ情報定数
-        const int MAP_OBJECT_FLOOR = 0;     //床　通行可ダメ無
+        const int MAP_OBJECT_FLOOR = 0;     //床　通行可
         const int MAP_OBJECT_WALL = 1;      //岩、壁　通行不可
-        const int MAP_OBJECT_TRAP = 2;//罠　通行可ダメ有
-        const int MAP_OBJECT_ROCK = 3;//動く岩　通行不可移動可
+        const int MAP_OBJECT_TRAP = 2;      //罠　通行可ダメ有
+        const int MAP_OBJECT_ROCK = 3;      //動く岩　通行不可移動可
 
         //キー入力定数
         const int KEY_UP = 0;
@@ -84,6 +93,8 @@ namespace mea
 
         int mMapImage;              //マップ画像
         int mMapBackGround;         //マップ背景画像
+
+        int mMoveDistance = 1;   //
     };
 
 
