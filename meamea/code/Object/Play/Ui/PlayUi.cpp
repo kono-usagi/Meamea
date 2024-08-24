5be5e401e7569d420f875de8275310fbaa38f443
406-07\student <k>
2024-08-24 16:21:52 +0900

    PlayUiのエラー修正

--- meamea/code/Object/Play/Ui/PlayUi.cpp
+++ meamea/code/Object/Play/Ui/PlayUi.cpp
@@ -1,21 +1,16 @@
 #include "PlayUi.h"
-#include "Player.h"
 
 namespace mea
 {
     PlayUi::PlayUi()
     {
-        //画像の読み込み
-        //BackGround = LoadGraph("");
-        //Stage = LoadGraph("");
-
         //フォントのロード
-        LPCSTR  fontPath = "test";　　//読み込むフォントファイルのパス
-        if (AddFontResourceEx(fontPath, FR_PRIVATE, NULL) > 0) {}
+        mFontPath = "test";
+        if (AddFontResourceEx(mFontPath, FR_PRIVATE, NULL) > 0) {}
         else
         {
             //フォント読み込みエラー表示
-            MessegeBox(NULL, "フォント読み込み失敗", "", MB_OK);
+            MessageBox(NULL, "フォント読み込み失敗", "", MB_OK);
         }
 
         //文字サイズ変更
@@ -24,12 +19,8 @@ namespace mea
 
     PlayUi::~PlayUi()
     {
-        //画像の削除
-        //DeleteGraph(BackGround);
-        //DeleteGraph(Stage);
-
         //フォントのアンロード
-        if(RemoveFontResourceEx(fontPath, FR_PRIVATE, NULL)) { }
+        if(RemoveFontResourceEx(mFontPath, FR_PRIVATE, NULL)) { }
         else
         {
             //フォントのアンロードエラー表示   
@@ -39,19 +30,15 @@ namespace mea
 
     void PlayUi::Update()
     {
-        GameObj*player=GameObjectManager::GetFirstGameObj(ObjTag.Player);
+        //GameObj*player=GameObjectManager::GetFirstGameObj(ObjTag.Player);
         //現在の歩数取得
-        step = player->
+        //step = player->
     }
 
     void PlayUi::Draw()
     {
-        //背景画像の表示
-        //DrawGraph(0, 0, BackGround, TRUE);
-        //DrawGraph(0, 0, Stage, TRUE);
-        // 
         //歩数の表示
-        DrawString(0,0,step,GetColor(255,255,255));
+        DrawFormatString(0, 0, GetColor(255, 255, 255), "%d", step);
     }
 
 
