605a06952c13b7585eaa47accc5b752a84d32ab1
Koutexya <fumitan0129@gmail.com>
2024-09-20 11:48:19 +0900

    LilianDataの内容追加

--- meamea/code/JsonMaster/Text/LilianData/LilianData.h
+++ meamea/code/JsonMaster/Text/LilianData/LilianData.h
@@ -9,17 +9,29 @@ namespace mea
 		LilianData(Json json);
 		~LilianData();
 
-		std::string GetTalk() const { return mTalk; }
-		std::string GetChoice1A() const { return mChoice1A; }
-		std::string GetChoice1B() const { return mChoice1B; }
-		std::string GetChoice2A() const { return mChoice2A; }
-		std::string GetChoice2B() const { return mChoice2B; }
+		std::string GetTalk1() const { return mTalk1; }
+		std::string GetTalk1A() const { return mTalk1A; }
+		std::string GetTalk1B() const { return mTalk1B; }
+		std::string GetTalk2A() const { return mTalk2A; }
+		std::string GetTalk2B() const { return mTalk2B; }
+
+		std::string GetImage1() const { return mImage1; }
+		std::string GetImage1A() const { return mImage1A; }
+		std::string GetImage1B() const { return mImage1B; }
+		std::string GetImage2A() const { return mImage2A; }
+		std::string GetImage2B() const { return mImage2B; }
 
 	private:
-		std::string mTalk;//最初の会話
-		std::string mChoice1A;//選択肢一個目Aの会話
-		std::string mChoice1B;//選択肢一個目Bの会話
-		std::string mChoice2A;//選択肢二個目Aの会話
-		std::string mChoice2B;//選択肢二個目Bの会話
+		std::string mTalk1;//最初の会話
+		std::string mTalk1A;//選択肢一個目Aの会話
+		std::string mTalk1B;//選択肢一個目Bの会話
+		std::string mTalk2A;//選択肢二個目Aの会話
+		std::string mTalk2B;//選択肢二個目Bの会話
+
+		std::string mImage1;//mTalk1でのイラスト
+		std::string mImage1A;//mTalk1Aでのイラスト
+		std::string mImage1B;//mTalk1Bでのイラスト
+		std::string mImage2A;//mTalk2Aでのイラスト
+		std::string mImage2B;//mTalk2Bでのイラスト
 	};
 }
